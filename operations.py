c608af4c313bd67f0a2ffd00d933f7fc83bd2cf9
masonkash <mason.kash@sdsw.org>
2022-12-14 12:23:20 -0500

    first plot

--- operations.py
+++ operations.py
@@ -16,5 +16,33 @@ print (x)
 
 pi = np.pi
 print(pi)
-z = y**2 = (5+x) + 1 - pi
+z = y**2 + (5+x) + 1 - pi
 print(z)
+
+i = 1
+while i<11:
+    print(i)
+    i =i+1
+
+del i
+i = 1
+while i<11:
+    print(i * '*')
+    i = i+1
+
+names = ["steelers" , "ravens" , "browns" , "bengals"]
+print(names)
+print(names[0])
+print(names[-1])
+print(names[-2])
+print(names[-3])
+
+numbers = [1,2,3,4,5]
+
+for item in numbers:
+    print(item)
+
+i=0
+while i< len(numbers)
+    print(numbers[i])
+    i = i+1
