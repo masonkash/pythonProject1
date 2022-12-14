a583ff22d178cd91387632dc64c0c117bfbcc5c0
masonkash <mason.kash@sdsw.org>
2022-12-14 12:52:49 -0500

    first plot

---
+++ factorial.py
@@ -0,0 +1,14 @@
+num = input("What number? ")
+print(num + " is the number")
+print("yes thats the number")
+num = input ("45 is your number right?")
+print(" yes, i said 45 ")
+num = input (" ok thanks, im just making sure" )
+print( "thanks" )
+num = input ( "ok last time asking but its 45 right?")
+print( "yes")
+
+
+
+
+
