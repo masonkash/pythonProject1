4ebc112183f92ca2464793e61b85aa4473fee417
masonkash <mason.kash@sdsw.org>
2023-01-06 09:41:20 -0500

    first plot

---
+++ project2.py
@@ -0,0 +1,14 @@
+#secretnum = 9
+#guess = int(input("guess # between 0-10"))
+#while secretnum != guess:
+    #print("wrong! try again")
+    #guess = int(input("try again"))
+#print("you win")
+
+secretnum = 15
+guess = int(input("guess the missing vowel using its place in the alphabet"))
+while secretnum != guess:
+    print("wrong vowel number! try again")
+    guess = int(input("try again"))
+print("correct vowel number")
+
